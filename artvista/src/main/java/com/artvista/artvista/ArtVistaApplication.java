4947d7586b5b6dcec5ebaf65c770dceb623b2799
AlianBoyz <ninjaniladri@gmail.com>
2026-02-11 21:29:05 +0530

    First Commit

---
+++ artvista/src/main/java/com/artvista/artvista/ArtVistaApplication.java
@@ -0,0 +1,13 @@
+package com.artvista.artvista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtVistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtVistaApplication.class, args);
+	}
+
+}
---
+++ artvista/src/test/java/com/artvista/artvista/ArtVistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artvista.artvista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtVistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
