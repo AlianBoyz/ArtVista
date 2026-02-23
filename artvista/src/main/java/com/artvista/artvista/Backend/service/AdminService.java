4b79edf7b34191958f18e08963d9f057164401e6
SenticHub <senargha679@gmail.com>
2026-02-23 18:21:14 +0530

    admin logic

---
+++ artvista/src/main/java/com/artvista/artvista/Backend/service/AdminService.java
@@ -0,0 +1,5 @@
+package com.artvista.artvista.Backend.service;
+
+public interface AdminService {
+    boolean isAdmin(String email, String password);
+}
--- artvista/src/main/java/com/artvista/artvista/Backend/service/service_implementation/AdminServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.artvista.artvista.Backend.service.service_implementation;
-
-public class AdminServiceImpl {
-    
-}
